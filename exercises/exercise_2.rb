d3e6d9d9e2bf80d1e584014bf6ed335c6e14b902
Seamus McGill <seamusmcgill@gmail.com>
2022-03-07 16:34:50 -0500

    Create variables for stores and update name of 1

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -5,3 +5,9 @@ puts "Exercise 2"
 puts "----------"
 
 # Your code goes here ...
+@store1 = Store.find_by(id: 1)
+# puts `Store 1 is #{@store1}`
+@store2 = Store.find_by(id: 2)
+# puts `Store 2 is #{@store2}`
+@store1.update(name: 'Coal Harbour')
+Store.all
